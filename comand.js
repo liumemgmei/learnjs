250b74be33433088bdd0d52a3873c77fe2697524
liumemgmei <18768147259@163.com>
2021-12-18 16:21:21 +0800

    初始化项目

---
+++ comand.js
@@ -0,0 +1,22 @@
+
+const fs = require('fs');
+let rawdata = fs.readFileSync('config.json');
+
+let config = JSON.parse(rawdata);
+
+config.files.forEach(element => {
+    require(element)
+});
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (line) => {
+  if(line === 's'){
+    rl.question('请输入要执行的js文件: ', name=>{
+      fs.writeFileSync('config.json', JSON.stringify({...config, files: [`./${name}.js`]}));
+    })
+  }
+});
